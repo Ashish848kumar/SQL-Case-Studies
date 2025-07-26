5a3f70fac617f4332ff14907dfe24a0b79019b44
ASHISH KUMAR <pspgamerid@gmail.com>
2025-07-26 10:45:23 +0530

    Update Histogram of Tweets.sql

--- SELECT/Histogram of Tweets.sql
+++ SELECT/Histogram of Tweets.sql
@@ -2,11 +2,11 @@
 # Output the tweet count per user as the bucket and the number of Twitter users who fall into that bucket.
 # In other words, group the users by the number of tweets they posted in 2022 and count the number of users in each group.
 # tweets Table:
-Column Name	Type
-tweet_id	integer
-user_id	integer
-msg	string
-tweet_date	timestamp
+Column Name	  Type
+tweet_id	    integer
+user_id	      integer
+msg	          string
+tweet_date	  timestamp
 
-Query 
+#Query:
 select user_id, count(tweet_id) as tweet_count_per_user from tweets where tweet_date BETWEEN '2022-01-01' AND'2022-12-31' group by user_id;
